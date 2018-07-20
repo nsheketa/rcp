d2735c74d37d5097e29775155c944c219968b382
shenaan <shenaan@gmail.com>
2018-07-21 00:29:13 +0300

    started working on menu's interactivity

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
 
+  lineLength();
+  iconsVisibility();
+  $(window).resize(function () {
+    lineLength();
+  });
+  $(window).scroll(function () {
+    iconsVisibility();
+  });
+
+  //header nav lines
   function lineLength() {
-    var line= $('.header__line');
+    var line= $('.header-nav__line');
 
     line.each(function() {
     $(this).css({
@@ -10,9 +20,91 @@ $(document).ready(function () {
     });
   }
 
-  lineLength();
-  $(window).resize(function () {
-    lineLength();
+  //header line navigation
+ $('.header__line').mouseover(function () {
+   $('.header__line-icon').addClass('visible');
+   $('.header-nav').addClass('fixed');
+ });
+
+  $('.header__line').mouseout(function () {
+    $('.header__line-icon').removeClass('visible');
+    $('.header-nav').removeClass('fixed');
   });
+
+  $('.header__line-icon,.header__line-link').mouseover(function () {
+    $('.header__line-link').addClass('visible');
+  });
+
+  $('.header__line-icon, .header__line-link').mouseout(function () {
+    $('.header__line-link').removeClass('visible');
+  });
+
+  //fixed elements visibility
+
+  function iconsVisibility() {
+    var header = $('.header'),
+        icons = $('.header__line-link'),
+        headerTop = header.offset().top,
+        headerHeight = parseInt(header.css('height')),
+        headerBottom = headerTop + headerHeight,
+        windowTop = $(window).scrollTop(),
+        windowHeight = window.innerHeight,
+        footer = $('.footer'),
+        footerTop = footer.offset().top,
+        headerLineR = $('.header__line-right'),
+        headerLineRBottom = headerLineR.offset().top + parseInt(headerLineR.css('height'));
+
+    if (headerTop >= windowTop && headerBottom <= windowHeight){
+      $('.header__line-icon').addClass('visible');
+      $('.header-nav').addClass('fixed');
+    }
+    else if (headerLineRBottom >= footerTop){
+      $('.header__line-icon').addClass('visible');
+      $('.header-nav').addClass('fixed');
+
+    }
+    else{
+      $('.header__line-icon').removeClass('visible');
+      $('.header-nav').removeClass('fixed');
+    }
+  }
+
+  //menu
+
+  $('.header__line-menu-link').click(function (e) {
+    e.preventDefault();
+    $('.menu').addClass('menu--open');
+    $('body').addClass('no-scroll');
+  });
+
+  $('.menu__close').click(function () {
+    $('.menu').removeClass('menu--open');
+    $('body').removeClass('no-scroll');
+  });
+
+  $('.menu-list__link-design').click(function (e) {
+    e.preventDefault();
+    $('.menu-sublist').fadeToggle();
+  });
+
+  $('.mob-menu__btn').click(function () {
+    $('.menu').addClass('menu--open')
+    $('body').addClass('no-scroll');
+  });
+
+
+
+  // smooth scrolling
+  $('.header__line-scroll-link').click(function () {
+
+    var the_id = $(this).attr("href");
+
+    $('html, body').animate({
+      scrollTop: $(the_id).offset().top
+    }, 'slow');
+
+    return false;
+  });
+
 
 });
